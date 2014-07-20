1f3ef16d77d3b9ff779672952011c5c8389b7c5b
Andy <andy@teamrubber.com>
2014-07-20 10:44:05 +0100

    Codechange: link in a new way without requiring firs.pnml (only works for TEST_INDUSTRY=lime_kiln)

--- src/render_pnml.py
+++ src/render_pnml.py
@@ -26,6 +26,9 @@ from cargos import registered_cargos
 import industries
 from industries import registered_industries
 
+# get args passed by makefile
+repo_vars = utils.get_repo_vars(sys)
+
 from chameleon import PageTemplateLoader # chameleon used in most template cases
 # setup the places we look for templates
 templates = PageTemplateLoader(os.path.join(src_path, 'templates'), format='text')
@@ -38,9 +41,7 @@ if not os.path.exists(generated_pnml_path):
 generated_nml_path = os.path.join(firs.generated_files_path, 'nml')
 if not os.path.exists(generated_nml_path):
     os.mkdir(generated_nml_path)
-
-# get args passed by makefile
-repo_vars = utils.get_repo_vars(sys)
+grf_nml = codecs.open(os.path.join(firs.generated_files_path, 'firs.nml'),'w','utf8')
 
 
 def render_nml(filename):
@@ -59,11 +60,12 @@ def render_nml(filename):
 
 
 def render_industry(industry):
-    # save the results of templating
-    pnml_file = codecs.open(os.path.join(generated_pnml_path, industry.id + '.pnml'), 'w','utf8')
-    pnml_file.write(industry.render_pnml())
-    pnml_file.close()
-    render_nml(industry.id)
+    if repo_vars.get('test_industry', None) == industry.id:
+        # save the results of templating
+        pnml_file = codecs.open(os.path.join(generated_pnml_path, industry.id + '.pnml'), 'w','utf8')
+        pnml_file.write(industry.render_pnml())
+        pnml_file.close()
+        render_nml(industry.id)
 
 
 def render_header_item_pnml(header_item):
@@ -73,6 +75,18 @@ def render_header_item_pnml(header_item):
     pnml.write(utils.unescape_chameleon_output(template(registered_industries=registered_industries, global_constants=global_constants,
                                                 utils=utils, sys=sys, generated_pnml_path=generated_pnml_path)))
     pnml.close()
+
+
+def link_nml(item, dep_path, split=None):
+    #dep_timestamps_new[dep_path] = os.stat(dep_path).st_mtime
+    item_nml = codecs.open(os.path.join('generated', 'nml', item + '.nml'),'r','utf8').read()
+    """
+    if split is not None:
+        # fragile split on some specific nfo, may break; assumes a right-split only
+        if split in item_nfo:
+            item_nfo = item_nfo.split(split)[1]
+    """
+    grf_nml.write(item_nml)
 
 
 def main():
@@ -111,27 +125,18 @@ def main():
         render_nml(header_item)
 
     # linker
-    print "Linking"
-    template = header_item_templates['firs.pypnml']
-    firs_pnml = codecs.open(os.path.join(firs.generated_files_path, 'firs.pnml'), 'w','utf8')
-    firs_pnml.write(utils.unescape_chameleon_output(template(registered_industries=registered_industries, global_constants=global_constants,
-                                                utils=utils, sys=sys, generated_pnml_path=generated_pnml_path)))
-    firs_pnml.close()
-
-    render_header_item_pnml('firs')
-    gcc_call_args = ['gcc',
-                      '-D',
-                      'REPO_REVISION='+str(repo_vars['repo_version']),
-                      '-C',
-                      '-E',
-                      '-nostdinc',
-                      '-x',
-                      'c-header',
-                      'generated/firs.pnml',
-                      '-o',
-                      'generated/firs.nml']
-    subprocess.call(gcc_call_args)
-
+    print "Linking nml"
+    for header_item in header_items:
+        link_nml(header_item, header_item, split=None)
+    for industry in industries.registered_industries:
+        if repo_vars.get('test_industry', None) == industry.id:
+            link_nml(industry.id, industry.id, split=None)
+    grf_nml.close()
+    """
+    dep_timestamps_file = codecs.open(dep_timestamps_path, 'w', 'utf8')
+    dep_timestamps_file.write(json.dumps(dep_timestamps_new))
+    dep_timestamps_file.close()
+    """
 
 if __name__ == '__main__':
     main()
